a2eda74c638ac2f0a1ecf2a74b5dae1ebcd5d08d
Oxana Odegova <oxyapple@yandex.ru>
2020-11-25 20:23:02 +0100

    search

--- src/main/java/com/odegova/angular/config/MyDataRestConfig.java
+++ src/main/java/com/odegova/angular/config/MyDataRestConfig.java
@@ -2,13 +2,29 @@ package com.odegova.angular.config;
 
 import com.odegova.angular.entity.Product;
 import com.odegova.angular.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.nio.channels.ClosedSelectorException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -21,6 +37,14 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.getExposureConfiguration().forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        Class[] domainTypes = entities.stream().map(EntityType::getJavaType).toArray(Class[]::new);
+        config.exposeIdsFor(domainTypes);
     }
 }
 
